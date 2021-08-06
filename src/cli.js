c3f24d48020ab03c2222157a516d35da304da429
Olga Kova <olgavpj@gmail.com>
2021-08-06 22:46:25 +0300

    Качество кода: - добавит CodeClimate - добавит ESLint

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-    const name = readlineSync.question('May I have your name?: ');
-    console.log(`Hello, ${name}!`)
-}
-
+export default () => {
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
+};
